d7562320ec6b4d969542afaeff876b34ac34f4bc
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2025-02-07 21:55:16 -0500

    Implementación del proyecto Counter App, básico en React

--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -1,10 +1,10 @@
+import './styles.css'
+import { CounterApp } from './CounterApp'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <CounterApp value={10} />
+    </React.StrictMode>
 )
