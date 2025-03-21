b1bc7a8acbfad0f8a0eaac0d03d4687887deed6e
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2025-03-21 12:31:19 -0500

    Creación y estructura de proyecto Heroes SPA App

---
+++ 06-heroes-spa-app/src/main.jsx
@@ -0,0 +1,10 @@
+import './styles.css'
+import { HeroesApp } from './HeroesApp'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HeroesApp />
+  </React.StrictMode>,
+)
