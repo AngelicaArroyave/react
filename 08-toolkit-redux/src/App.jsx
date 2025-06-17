5877a7e9e71b3fc6c311f57f1981faa627ab7c73
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2025-06-17 09:53:42 -0500

    Implementación básica de Redux y Redux Toolkit

--- 08-toolkit-redux/src/App.jsx
+++ 08-toolkit-redux/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { increment, decrement, incrementBy } from './store/slices/counter/counterSlice'
+import { useDispatch, useSelector } from 'react-redux'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const { counter } = useSelector(state => state.counter)  
+  const dispatch = useDispatch()
 
   return (
     <>
@@ -18,9 +21,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <p>Count is {counter}</p>
+        <p>
+          <button onClick={() => dispatch(increment())}>Increment</button>
+          <button onClick={() => dispatch(decrement())}>Decrement</button>
+          <button onClick={() => dispatch(incrementBy(2))}>Increment by 2</button>
+        </p>
       </div>
     </>
   )
