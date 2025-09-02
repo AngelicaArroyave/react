c0b82e11f3e46ef22365ff4e835fdc5fc613d89c
Angelica Arroyave <aarroyaveduque20@gmail.com>
2025-09-02 09:25:07 -0500

    Finalización de proyecto

--- 02-gifs-app-2025/src/GifsApp.tsx
+++ 02-gifs-app-2025/src/GifsApp.tsx
@@ -1,42 +1,48 @@
-import { mockGifs } from './mock-data/gifs.mock'
+import { useState } from 'react'
+
+// import { mockGifs } from './mock-data/gifs.mock'
+import { CustomHeader } from './shared/components/CustomHeader'
+import { getGifsByQuery } from './gifs/actions/get-gifs-by-query.action'
+import { GifList } from './gifs/components/GifList'
+import { PreviousSearches } from './gifs/components/PreviousSearches'
+import { SearchBar } from './shared/components/SearchBar'
+import type { Gif } from './gifs/interfaces/gif.interface'
 
 export const GifsApp = () => {
+    const [previousTerms, setPreviousTerms] = useState<string[]>([])
+    const [searchedGifs, setSearchedGifs] = useState<Gif[]>([])
+
+    const handleTermClicked = (term: string) => {
+        console.log("🚀 ~ handleTermCliked ~ term:", term)
+    }
+
+    const handleSearch = async (query: string) => {
+        query = query.toLowerCase().trim()
+
+        if (!query) return
+
+        if (previousTerms.includes(query)) return
+
+        setPreviousTerms([query, ...previousTerms].slice(0, 8))
+
+        const gifs = await getGifsByQuery(query)
+        
+        setSearchedGifs(gifs)
+    }
+
     return (
         <>
             {/* Header */}
-            <div className="content-center">
-                <h1>Gifs Search</h1>
-                <p>Discover and share the perfect Gif</p>
-            </div>
+            <CustomHeader title='Gifs Search' description='Discover and share the perfect Gif' />
 
             {/* Search */}
-            <div className="search-container">
-                <input type="text" placeholder="Search for Gifs" />
-                <button>Search</button>
-            </div>
+            <SearchBar placeholder="Search for whatever you want" onQuery={handleSearch} />
 
             {/* Previous searches */}
-            <div className="previous-searches">
-                <h2>Previous searches</h2>
-                <ul className="previous-searches-list">
-                    <li>Goku</li>
-                    <li>Saitama</li>
-                    <li>Elden Ring</li>
-                </ul>
-            </div>
+            <PreviousSearches searches={previousTerms} onLabelClicked={handleTermClicked} />
 
             {/* Gifs */}
-            <div className="gifs-container">
-                {
-                    mockGifs.map(gif => (
-                        <div key={ gif.id } className="gif-card">
-                            <img src={ gif.url } alt={ gif.title } />
-                            <h3>{ gif.title }</h3>
-                            <p>{ gif.width } x { gif.height } (1.5mb)</p>
-                        </div>
-                    ))
-                }
-            </div>
+            <GifList gifs={searchedGifs} />
         </>
     )
 }
